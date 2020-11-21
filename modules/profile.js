1957689a26064844bb30b167769f4325805bb417
Daniel Jones <jonesd37@coventry.ac.uk>
2020-11-21 09:06:01 +0000

    Stage 1 part 1 functionality completed
    I have created three new files; the profile.js module, profile_route.js route,and profile.handlebars. I have also updated the users table to contain the new required fields for the user profile.
    
    This has involved creating new functions within the modules to achieve the functionality, such as an update data function to handle adding or changing user profile information.
    
    To do: consider the efficiency of the newly added functions and work on the error handling handlebars file to be more specific.

---
+++ modules/profile.js
@@ -0,0 +1,62 @@
+
+/** @module profile */
+
+import sqlite from 'sqlite-async'
+
+/**
+ *
+ * ES6 module that manages the user profile, tied to the users table in the Sandwich Ordering Service system.
+ */
+class Profile {
+	/**
+   * Create a profile object
+   * @param {String} [dbName=":memory:"] - The name of the database file to use.
+   */
+	constructor(dbName = ':memory:') {
+		return (async() => {
+			this.db = await sqlite.open(dbName)
+			const sql = 'CREATE TABLE IF NOT EXISTS users(\
+						id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT,\
+						pass TEXT, email TEXT, firstName TEXT, lastName TEXT,\
+						company TEXT, addressLine1 TEXT, addressLine2 TEXT,\
+						city TEXT, postcode TEXT);'
+			await this.db.run(sql)
+			return this
+		})()
+	}
+
+	/**
+	 * Returns the relevant data from the users table queried by the user id
+	 * @params {Number} - the id of a user
+	 * @returns {Boolean} - returns an array containing all of the user details for
+	 */
+	async all(userid) {
+		const sql = `SELECT * FROM users WHERE id = ${userid};`
+		const profile = await this.db.all(sql)
+		return profile
+	}
+	/**
+	 * Update profile related user data by the id of the user
+	 * @params {Object} - json object (request body)
+	 * @params {Number} - the id of a user
+	 * @returns {Boolean} - returns true upon success
+	 */
+	async add(data, userid) {
+		try {
+			const sql = `UPDATE users SET firstName = "${data.firstName}",\
+						lastName = "${data.lastName}", company = "${data.company}",\
+						addressLine1 = "${data.addressLine1}", addressLine2 = "${data.addressLine2}",\
+						city = "${data.city}", postcode = "${data.postcode}" WHERE id = ${userid};`
+			await this.db.run(sql)
+			return true
+		} catch(err) {
+			console.log(err)
+			throw err
+		}
+	}
+	async close() {
+		await this.db.close()
+	}
+}
+
+export default Profile
